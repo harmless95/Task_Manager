461ea3bca801e3ac7d2302713035e13e94ea9927
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-22 12:29:01 +0700

    fix name id

--- ProjectTaskManager/core/model/mixins/uuid_pr_key.py
+++ ProjectTaskManager/core/model/mixins/uuid_pr_key.py
@@ -5,7 +5,7 @@ from sqlalchemy.dialects.postgresql import UUID as UUID_PG
 
 
 class KeyUUID:
-    uuid_k: Mapped[UUID] = mapped_column(
+    id: Mapped[UUID] = mapped_column(
         UUID_PG(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
