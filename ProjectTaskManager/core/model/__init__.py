2863d3f5a8d6b3605f4c24b32fbf1389ce4f0be4
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-22 12:31:03 +0700

    added to scope

---
+++ ProjectTaskManager/core/model/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "Base",
+    "db_helper",
+    "Address",
+    "AddressBrands",
+    "Brand",
+    "Car",
+)
+
+from .base import Base
+from .helper_database import db_helper
+from .address import Address
+from .address_brands import AddressBrands
+from .brand_car import Brand
+from .car import Car
