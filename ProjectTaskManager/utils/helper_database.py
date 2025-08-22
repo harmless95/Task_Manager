0c9f90c25162c30f403144239739fa27f6c4c966
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-22 11:00:24 +0700

    create helper_database

---
+++ ProjectTaskManager/utils/helper_database.py
@@ -0,0 +1,46 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from core.config import setting
+
+
+class HelperDatabase:
+    def __init__(
+        self,
+        url: str,
+        echo: bool,
+        echo_pool: bool,
+        pool_size: int,
+        max_overflow: int,
+    ):
+        self.async_engine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.fabric_session = async_sessionmaker(
+            bind=self.async_engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.async_engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.fabric_session() as session:
+            yield session
+            await session.close()
+
+db_helper = HelperDatabase(
+    url=str(setting.db.url),
+    echo=setting.db.echo,
+    echo_pool=setting.db.echo_pool,
+    pool_size=setting.db.pool_size,
+    max_overflow=setting.db.max_overflow,
+)
