70c5fee4272754564b07c41ac551edb15b77555c
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-22 13:26:24 +0700

    create get car and add selectinload

--- ProjectTaskManager/api/CRUD/crud_car.py
+++ ProjectTaskManager/api/CRUD/crud_car.py
@@ -1,5 +1,8 @@
+from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from sqlalchemy import select
+from fastapi import HTTPException, status
 
 from core.model import Car
 
@@ -7,7 +10,19 @@ from core.model import Car
 async def get_cars(
     session: AsyncSession,
 ) -> list[Car]:
-    stmt = select(Car).order_by(Car.id)
+    stmt = select(Car).options(selectinload(Car.brand_name)).order_by(Car.id)
     result = await session.scalars(stmt)
     cars = result.all()
     return list(cars)
+
+
+async def get_car(session: AsyncSession, car_id: UUID) -> Car:
+    stmt = select(Car).options(selectinload(Car.brand_name)).where(Car.id == car_id)
+    result = await session.scalars(stmt)
+    car = result.first()
+    if not car:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Invalid id: {car_id!r} not found",
+        )
+    return car
--- ProjectTaskManager/api/routers/car.py
+++ ProjectTaskManager/api/routers/car.py
@@ -1,8 +1,9 @@
 from typing import Annotated
+from uuid import UUID
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.CRUD.crud_car import get_cars
+from api.CRUD.crud_car import get_cars, get_car
 from core.config import setting
 from core.model import db_helper, Car
 from core.schema.schema_car import CarRead
@@ -23,3 +24,16 @@ async def get_all_cars(
 ) -> list[Car]:
     cars = await get_cars(session=session)
     return cars
+
+
+@router.get(
+    "/{car_id}/",
+    response_model=CarRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_car_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    car_id: UUID,
+) -> Car:
+    car = await get_car(session=session, car_id=car_id)
+    return car
