9a65e0b57b3c369760c3173b016fda006aa07e66
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-22 11:11:11 +0700

    create mixins uuid

---
+++ ProjectTaskManager/core/model/mixins/uuid_pr_key.py
@@ -0,0 +1,12 @@
+import uuid
+from uuid import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID as UUID_PG
+
+
+class KeyUUID:
+    uuid_k: Mapped[UUID] = mapped_column(
+        UUID_PG(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
